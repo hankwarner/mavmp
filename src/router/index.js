5bb5b2246f888f92ae0e91d6fec1c73987cd673d
Hank Warner <HWarner@angeloaklending.com>
2021-03-04 17:35:22 -0500

    WIP

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Landing from "@/components/Landing";
+import AboutUs from "@/components/AboutUs";
+import Company from "@/components/Company";
+import ContactUs from "@/components/ContactUs";
+import RequestOffer from "@/components/RequestOffer";
+import Testimonials from "@/components/Testimonials";
+
+Vue.use(Router);
+
+export default new Router({
+	routes: [
+		{
+			path: "/",
+			component: Landing,
+		},
+		{
+			path: "/about-us",
+			component: AboutUs,
+		},
+		{
+			path: "/company",
+			component: Company,
+		},
+		{
+			path: "/contact-us",
+			component: ContactUs,
+		},
+		{
+			path: "/request-offer",
+			component: RequestOffer,
+		},
+		{
+			path: "/testimonials",
+			component: Testimonials,
+		},
+	],
+	mode: "history",
+	// Scroll to top of page when transitioning between components in the router view
+	scrollBehavior() {
+		return { x: 0, y: 0 };
+	},
+});
